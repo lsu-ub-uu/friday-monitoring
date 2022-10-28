d67d251f44b0e4a5ac40c5db442e6c84334514d2
pere <pere@a42e5052b070>
2022-10-28 07:46:03 +0000

    add some extra dots

--- WebContent/script/repoTagsToken.js
+++ WebContent/script/repoTagsToken.js
@@ -16,5 +16,5 @@
  *     You should have received a copy of the GNU General Public License
  *     along with Friday.  If not, see <http://www.gnu.org/licenses/>.
  */
-var yourToken = "your token here";
+var yourToken = "your token here...";
 var repoToken = "token " + yourToken;
